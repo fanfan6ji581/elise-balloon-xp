241e75593548b89efe73580dd8ca5b20ac89400a
Xinyu Li <xinyuli@uber.com>
2023-03-08 23:31:45 +1100

    updaet library

--- src/components/attendant/pretask/BallOption.js
+++ src/components/attendant/pretask/BallOption.js
@@ -1,41 +1,45 @@
 import { React, useState } from "react";
 import { Box, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles'
 import BallOptionChart from "./BallOptionChart"
+import { Global, css } from '@emotion/react'
 
-const useStyles = makeStyles({
-    card: {
-        backgroundColor: '#fff',
-        '&:hover': {
-            backgroundColor: 'rgba(229, 228, 226, 0.4)',
-        },
-    },
-    cardactive: {
-        backgroundColor: 'rgba(100, 149, 237, 0.25)!important',
-    }
-})
 
 export default function BallOption({ type, winQty, lossQty, winCash, lossCash, label }) {
-    const classes = useStyles()
     const [active, setActive] = useState(false)
 
     const onClick = () => {
         setActive(!active);
     }
 
-    return <Box sx={{ p: 1, }}>
-        <Box className={`${classes.card} ${active ? classes.cardactive : ''}`} onClick={() => onClick()}>
-            <BallOptionChart
-                type={type}
-                winQty={winQty}
-                lossQty={lossQty}
-                winCash={winCash}
-                lossCash={lossCash}
-            />
+    return <>
+        <Global
+            styles={css`
+                .card {
+                    background-color: #fff
+                }
+                .card:hover {
+                    background-color: rgba(229, 228, 226, 0.4)
+                }
+                .card.cardactive {
+                    background-color: rgba(100, 149, 237, 0.25)
+                }
+            `}
+        />
 
-            <Box sx={{ textAlign: 'center' }}>
-                <Typography variant="h4">{label}</Typography>
+        <Box sx={{ p: 1, }}>
+            <Box className={`card ${active ? 'cardactive' : ''}`} onClick={() => onClick()}>
+                <BallOptionChart
+                    type={type}
+                    winQty={winQty}
+                    lossQty={lossQty}
+                    winCash={winCash}
+                    lossCash={lossCash}
+                />
+
+                <Box sx={{ textAlign: 'center' }}>
+                    <Typography variant="h4">{label}</Typography>
+                </Box>
             </Box>
         </Box>
-    </Box>
+    </>
 }
--- src/components/attendant/pretask/BallOptionSkip.js
+++ src/components/attendant/pretask/BallOptionSkip.js
@@ -1,22 +1,8 @@
 import { React, useState } from "react";
 import { Box, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles'
 import BallOptionChartSkip from "./BallOptionChartSkip"
 
-const useStyles = makeStyles({
-    card: {
-        backgroundColor: '#fff',
-        '&:hover': {
-            backgroundColor: 'rgba(229, 228, 226, 0.4)',
-        },
-    },
-    cardactive: {
-        backgroundColor: 'rgba(100, 149, 237, 0.3)!important',
-    }
-})
-
 export default function BallOptionSkip() {
-    const classes = useStyles()
     const [active, setActive] = useState(false)
 
     const onClick = () => {
@@ -24,7 +10,7 @@ export default function BallOptionSkip() {
     }
 
     return <Box sx={{ p: 1, }}>
-        <Box className={`${classes.card} ${active ? classes.cardactive : ''}`} onClick={() => onClick()}>
+        <Box className={`card ${active ? 'cardactive' : ''}`} onClick={() => onClick()}>
             <BallOptionChartSkip />
             <Box sx={{ textAlign: 'center' }}>
                 <Typography variant="h4">Skip</Typography>
