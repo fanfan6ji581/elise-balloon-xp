954a395566a24e3c0c7e5ddc70fc020a8a58bf70
Xinyu Li <xinyuli@uber.com>
2023-03-13 22:25:15 +1100

    update

--- src/components/attendant/trial/TrainingTimer.js
+++ src/components/attendant/trial/TrainingTimer.js
@@ -12,16 +12,9 @@ const TrainingTimer = ({ trainingSessionSeconds, onFinish }) => {
         <>
             {
                 <Container maxWidth="lg" sx={{ mt: 3 }}>
-                    <Grid container alignItems="center">
-                        <Grid item xs={6}>
-                            <Typography variant="body1" textAlign="right" sx={{ mr: 1 }}>
-                                Training time left:
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Countdown date={Date.now() + trainingSessionSeconds * 1000} renderer={renderer} onComplete={onFinish} />
-                        </Grid>
-                    </Grid>
+                    <Typography variant="body1" textAlign="center" sx={{ mr: 1 }}>
+                        Training time left: <Countdown date={Date.now() + trainingSessionSeconds * 1000} renderer={renderer} onComplete={onFinish} />
+                    </Typography>
                 </Container>
             }
         </>
